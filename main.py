74bb2025bd7ef426476c688682291252906a7b85
Artur <aat281205@gmail.com>
2022-02-14 18:52:57 +0300

    new_commit

--- main.py
+++ main.py
@@ -6,6 +6,11 @@ from wtforms.validators import DataRequired
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'really_secret_config_key'
 
+params = {'title': 'захват марса',
+          'header': 'Миссия Колонизация Марса!',
+          'text': 'И на Марсе будут яблони цвести!',
+          'source': 'static/css/style.css'}
+
 
 class LoginForm(FlaskForm):
     username = StringField('Логин', validators=[DataRequired()])
@@ -13,15 +18,13 @@ class LoginForm(FlaskForm):
     remember_me = BooleanField('Запомнить меня')
     submit = SubmitField('Войти')
 
+
 @app.route('/')
 @app.route('/index')
 @app.route('/<string:title>')
 @app.route('/index/<string:title>')
 def index(title='заголовок не указан'):
-    params = {'title': title,
-              'header': 'Миссия Колонизация Марса',
-              'text': 'И на Марсе будут яблони цвести!',
-              'href': f"{url_for('static', filename='css/style.css')}"}
+    params['title'] = title
     return render_template('base.html', **params)
 
 
@@ -36,6 +39,26 @@ def login():
 @app.route('/succes')
 def succes_login_up():
     return redirect('/index')
+
+
+@app.route('/training/<prof>')
+@app.route('/training')
+def profession(prof=None):
+    if not prof:
+        return redirect('/')
+    if any(i in prof for i in ('инженер', 'строитель')):
+        params['img'] = url_for('static', filename='img/worker.png')
+        profes = prof.split()
+        for i in prof:
+            if not any(j in i for j in ('инженер', 'строитель')):
+                profes = i
+            break
+        params['text1'] = f'тренажёры по специальности {profes}'
+    else:
+        params['img'] = url_for('static', filename='img/science.png')
+        params['text1'] = 'научные стимуляторы'
+    params['source2'] = '../' + params['source']
+    return render_template('prof.html', **params)
 
 
 if __name__ == '__main__':
